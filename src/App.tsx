3a8abd547028a2923c5b8a7e9d3dc409ae716432
Александр <80578847+PiramEVR@users.noreply.github.com>
2022-04-25 21:00:06 +0300

    config esLint, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import s from './App.module.css';
+import { ReturnComponentType } from './types/ReturnComponentType';
+
+const App = (): ReturnComponentType => <div className={s.app}>+</div>;
 
 export default App;
